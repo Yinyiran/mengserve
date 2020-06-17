aeab591aec2df817fccad048374759f1eff623b2
chrisgad <chrisgad@qq.com>
2020-06-17 18:17:17 +0800

    BanType 改为int

--- meng Session.sql
+++ meng Session.sql
@@ -50,8 +50,10 @@ alter table banner modify BanImg varchar(255) not null;
 alter table banner change BanType BanType int(1) default 1;
 alter table banner auto_increment = 10000;
 alter table banner drop BanIntro ;
+alter table banner modify BanType int(1) not null ;
 
-update banner set BanImg = "resource/img/2020-06-03/1591175135950.jpg"
+update banner set BanType = 2 where banid in (10009,10011)
+update banner set BanType = 1 where banid in (10010)
 
 select * from banner;
 insert into banner (BanTargID,BanImg) values (123134,"resource/img/2020-06-03/1591175135950.jpg")
